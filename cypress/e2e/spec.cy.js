3188ae8ce31c95cbb0586925276df5856830a998
unknown <vlad.cpp@gmail.com>
2022-09-07 19:42:34 +0300

    adding more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,7 @@ describe('Movies', () => {
     cy.visit('/')
     cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
       .should('contain', 'WALL·E')
+    cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
+      .should('be.visible')
   })
 })
