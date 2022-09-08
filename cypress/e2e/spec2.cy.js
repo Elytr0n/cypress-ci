61a6d1af75842289a3fb78900e382f70c6d95acd
unknown <vlad.cpp@gmail.com>
2022-09-08 17:56:42 +0300

    changing second test

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,8 +1,8 @@
-describe('Movies', () => {
+describe('Second suite', () => {
   it('first', () => {
     cy.visit('/')
     cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
-      .should('contain', 'WALL·E')
+      .should('contain', 'WALL·E$')
     cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
       .should('be.visible')
       //test
