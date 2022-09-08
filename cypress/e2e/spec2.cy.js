7e2b8f34ed6c14dc3f8bf55f26a1213d1b952fd4
unknown <vlad.cpp@gmail.com>
2022-09-08 21:08:40 +0300

    fixing naming error

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,10 +1,39 @@
-describe('Second suite', () => {
-  it('first', () => {
-    cy.visit('/')
-    cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
-      .should('contain', 'WALL·E$')
-    cy.get(':nth-child(1) > a.jsx-251689666 > h3.jsx-251689666')
-      .should('be.visible')
-      //test
+/// <reference types="cypress" />
+
+function userID_Alpha() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+  for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+let createAccBtnLoc = '[class="_btnFill_1hv0m_1"]';
+let emailValidationLoc = '[class="MuiFormHelperText-root MuiFormHelperText-sizeMedium MuiFormHelperText-filled _error_mdd7a_38 css-j7o63n"]';
+
+describe('Sign Up Test Suite', () => {
+
+  beforeEach( () => {
+      cy.visit('/sign-up');
   })
-})
+
+
+  it('TC15 - 1 symbol for the email local part required', () => {
+    cy.get('input[name="name"][type="text"]').type('NoLocalpart NoLove');
+    cy.get('input[name="email"][type="email"]').type('@gmail.com');
+    cy.get('input[name="password"][type="password"]').type('11111111');
+    cy.get('input[name="repeatPassword"][type="password"]').clear();
+
+    cy.contains(createAccBtnLoc,'Create Account').click()
+
+    cy.get('input[name="email"][type="email"]').invoke('prop', 'validationMessage')
+         .should('contain', 'Введите часть адреса до символа "@". Адрес "@gmail.com" неполный.');
+
+    cy.get('input[name="email"][type="email"]').clear();
+    cy.get('input[name="email"][type="email"]').type('a@gmail.com');
+    cy.get('input[name="email"][type="email"]').invoke('prop', 'validationMessage')
+        .should('be.empty');
+  })
+  }
+)
